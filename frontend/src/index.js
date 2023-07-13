31d9c7158903bf48a36a6d22819af83390a30ffa
Nicolas Ortiz <niortiz51@gmail.com>
2023-07-14 02:40:25 +1000

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Web3ReactProvider } from '@web3-react/core';
 import { getProvider } from './utils/provider';
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
